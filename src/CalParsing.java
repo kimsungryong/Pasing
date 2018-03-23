70917235d2fba4180e76ea1e0ef43d4c24796baf
KimSungRyong <KimSungRyong@DESKTOP-A8MH29Q>
2018-03-23 13:55:26 +0900

    데이터 입력

---
+++ src/CalParsing.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CalParsing {
+
+	public static void main(String[] args) {
+		// 데이터 파싱
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		System.out.println(input);
+
+	}
+
+}
--- src/CalPasing.java
+++
@@ -1,9 +0,0 @@
-
-public class CalPasing {
-
-	public static void main(String[] args) {
-		// 데이터 파싱
-
-	}
-
-}
