01a6f31ed151761c98d9e71a21a39a76f0219955
KimSungRyong <KimSungRyong@DESKTOP-A8MH29Q>
2018-03-26 10:47:56 +0900

    스티링토큰라이저로 파싱

--- src/CalParsing.java
+++ src/CalParsing.java
@@ -5,9 +5,20 @@ public class CalParsing {
 
 	public static void main(String[] args) {
 		// 데이터 파싱
+
+		System.out.println("0~100 이내의 정수를 사칙연산하시오");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		System.out.println(input);
+
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+			System.out.println("input.charAt(" + i + ") : " + ch);
+		}
+
+		char[] chArr = input.toCharArray();
+
+		System.out.println(chArr);
 
 		StringTokenizer st = new StringTokenizer(input, "+-*/=()", true);
 
